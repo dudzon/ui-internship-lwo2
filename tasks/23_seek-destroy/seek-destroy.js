e3a943b5d4b24d1dd1cb4e02806bd0dc5b2e991f
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-04-22 23:28:27 +0200

    Task 23

---
+++ tasks/23_seek-destroy/seek-destroy.js
@@ -0,0 +1,13 @@
+/* eslint-disable prefer-rest-params */
+
+// Arrow functions don't have an access to arguments object
+// so classic function must be used.
+// First, create an empty array and copy arguments with call method,
+// then filter out arguments.
+
+export function destroyer(arr) {
+  const args = Array.prototype.slice.call(arguments);
+  // Filtering the arguments from array
+  arr = arr.filter((item) => args.indexOf(item)==-1 ? true :false );
+  return arr;
+}
