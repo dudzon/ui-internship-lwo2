5f1311d6e5c050749cb284d0eadbeb4880009182
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-04-23 12:43:39 +0200

    Fixed spaces in task 29

--- tasks/29_remove-duplicates/remove-duplicates.js
+++ tasks/29_remove-duplicates/remove-duplicates.js
@@ -3,10 +3,10 @@
 // 1st solution : use filter and indexOf:
 
 // eslint-disable-next-line max-len
-// export const removeDuplicates = (arr) => arr.filter( (elem, index) => arr.indexOf(elem)===index);
+// export const removeDuplicates = ( arr ) => arr.filter( (elem, index) => arr.indexOf(elem) === index );
 
 // 2nd solution: Use Set, and that's all.
 // We could use spread operator too: [...new Set(arr)]
 
-export const removeDuplicates = (arr) =>Array.from(new Set(arr));
+export const removeDuplicates = (arr) => Array.from( new Set(arr) );
 
