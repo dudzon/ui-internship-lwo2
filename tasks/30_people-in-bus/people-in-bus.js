0fb12c8a76000942723c4d5670369e9a60c99022
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-04-23 12:46:23 +0200

    Fixed spaces in task 30

--- tasks/30_people-in-bus/people-in-bus.js
+++ tasks/30_people-in-bus/people-in-bus.js
@@ -1,11 +1,11 @@
 //  I'm repeating myself, probably it could be refactored
 //  but I don'know how :)
 
-export const peopleInBus = (arr) => {
-  const peopleIn = arr.map( (item) => item[0])
-      .reduce((prev, next) => prev + next, 0);
-  const peopleOut = arr.map( (item) => item[1])
-      .reduce( (prev, next) => prev + next, 0);
+export const peopleInBus = ( arr ) => {
+  const peopleIn = arr.map( ( item ) => item[0] )
+      .reduce( ( prev, next ) => prev + next, 0 );
+  const peopleOut = arr.map( ( item ) => item[1] )
+      .reduce( ( prev, next ) => prev + next, 0 );
 
   return peopleIn - peopleOut;
 };
