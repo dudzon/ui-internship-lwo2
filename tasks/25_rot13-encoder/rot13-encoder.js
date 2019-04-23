c69478dd445e8abc8efe5420ceb74866b2d508c7
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-04-23 12:34:44 +0200

    Fixed spaces in task 25

--- tasks/25_rot13-encoder/rot13-encoder.js
+++ tasks/25_rot13-encoder/rot13-encoder.js
@@ -2,16 +2,16 @@
 // uppercased letters is between 65 and 90.
 // We swap the charCodes by adding or substracting 13 from original charCode.
 
-export const rot13Encoder =(string) =>{
+export const rot13Encoder = ( string ) => {
   let result = [];
 
-  for ( let i = 0; i<= string.length; i++) {
-    if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <=77) {
-      result.push(String.fromCharCode(string.charCodeAt(i)+13));
-    } else if (string.charCodeAt(i)>=78 && string.charCodeAt(i) <=90) {
-      result.push(String.fromCharCode(string.charCodeAt(i)-13));
+  for ( let i = 0; i <= string.length; i++ ) {
+    if ( string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 77 ) {
+      result.push( String.fromCharCode( string.charCodeAt(i) + 13 ) );
+    } else if ( string.charCodeAt(i) >= 78 && string.charCodeAt(i) <= 90 ) {
+      result.push( String.fromCharCode( string.charCodeAt(i) - 13 ) );
     } else {
-      result.push(string[i]);
+      result.push( string[i] );
     }
   }
   return result.join('');
