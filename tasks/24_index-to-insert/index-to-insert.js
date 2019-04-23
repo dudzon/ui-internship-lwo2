9b5ea5df8f12e0b0da0ac5e6cf570e5b194dc6bb
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-04-23 12:30:09 +0200

    Fixed spaces in task 24 v2.0.

--- tasks/24_index-to-insert/index-to-insert.js
+++ tasks/24_index-to-insert/index-to-insert.js
@@ -1,5 +1,5 @@
 //  Sort array, and return the length of filtered elements
 //  that are lower than val.
 //  It means index number where val should be inserted into original array.
-export const getIndexToInsert = ( arr, val ) =>arr.sort( (a, b) => a-b )
+export const getIndexToInsert = ( arr, val ) => arr.sort( (a, b) => a-b )
     .filter( (elem) => elem <val ).length;
