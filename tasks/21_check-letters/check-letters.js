e1d8bff1bdf3a9294f70af8fe3d3b161b401c7d9
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-04-24 09:24:51 +0200

    Task 21 - code refactoring

--- tasks/21_check-letters/check-letters.js
+++ tasks/21_check-letters/check-letters.js
@@ -1,17 +1,28 @@
+// Original Solution:
 // Make an empty array, run indexOf() on first element of input array.
 // Make sure that case is ignored adding toLowerCase() and fill output
 // array with number of indices. Run array every method to find out
 // if all elements in output array > -1,(exist in input string)
 
+// export const checkLetters = ( arr ) =>{
+//   const result = [];
+//   const lowercased = arr[0].toLowerCase();
+
+//   for ( let item of arr[1].toLowerCase() ) {
+//     result.push( lowercased.indexOf(item) );
+//   }
+//   return result.every( (element) => element > -1 );
+// };
+
+// Second solution -   Ivan's suggestions
+
 export const checkLetters = ( arr ) =>{
-  const result = [];
   const lowercased = arr[0].toLowerCase();
 
   for ( let item of arr[1].toLowerCase() ) {
-    result.push( lowercased.indexOf(item) );
+    if (lowercased.indexOf(item) === -1) {
+      return false;
+    }
   }
-
-  return result.every( (element) => element > -1 );
+  return true;
 };
-
-
