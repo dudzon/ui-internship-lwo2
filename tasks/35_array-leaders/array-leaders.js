4ab692d46a3437c33de6d6160550267f6d302e09
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-04-26 13:39:52 +0200

    Code refactored - task 35

--- tasks/35_array-leaders/array-leaders.js
+++ tasks/35_array-leaders/array-leaders.js
@@ -5,7 +5,8 @@
 export const arrayLeaders = ( arr ) => {
   const leaders = [];
   for (let i = 0; i < arr.length; i++) {
-    if ( arr[i] > arr.slice(i + 1).reduce((prev, next) => prev + next, 0) ) {
+    const nextElemSum = arr.slice(i + 1).reduce((prev, next) => prev + next, 0);
+    if ( arr[i] > nextElemSum ) {
       leaders.push(arr[i]);
     }
   }
