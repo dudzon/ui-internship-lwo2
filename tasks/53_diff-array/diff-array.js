3c90eaae20cbc883b80e29a6e7fa06e372ec1b11
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-04-28 09:32:23 +0200

    Task 53 - second solution added

--- tasks/53_diff-array/diff-array.js
+++ tasks/53_diff-array/diff-array.js
@@ -1,16 +1,23 @@
 // It's not a smart solution :(
-export const diffArray = ( arr1, arr2 ) => {
-  let result = [];
+// export const diffArray = ( arr1, arr2 ) => {
+//   let result = [];
 
-  arr1.filter(( item ) => {
-    if (arr2.indexOf( item ) === -1) {
-      result.push( item );
-    }
-  });
-  arr2.filter(( item ) => {
-    if (arr1.indexOf( item ) === -1) {
-      result.push( item );
-    }
-  });
-  return result;
-};
+//   arr1.filter(( item ) => {
+//     if (arr2.indexOf( item ) === -1) {
+//       result.push( item );
+//     }
+//   });
+//   arr2.filter(( item ) => {
+//     if (arr1.indexOf( item ) === -1) {
+//       result.push( item );
+//     }
+//   });
+//   return result;
+// };
+
+// Solution 2 -  much better
+
+export const diffArray = ( arr1, arr2 ) => [...arr1, ...arr2]
+    // eslint-disable-next-line max-len
+    .filter(( elem, index, arr ) => arr.indexOf(elem) === arr.lastIndexOf(elem) );
+
