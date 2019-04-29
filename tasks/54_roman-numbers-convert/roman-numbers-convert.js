382b45720b9cfe0e74d644cfcd427a5fdc9eb1ac
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-04-29 18:47:03 +0200

    Task 54

---
+++ tasks/54_roman-numbers-convert/roman-numbers-convert.js
@@ -0,0 +1,29 @@
+export const convertToRoman = ( num ) => {
+  // eslint-disable-next-line max-len
+  const romanLetters = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let romanNumber = '';
+  let valuesArr = Object.values( romanLetters) ;
+
+  // eslint-disable-next-line guard-for-in
+  for ( let i in valuesArr ) {
+    while ( num - valuesArr[i] >= 0 ) {
+      romanNumber += Object.keys( romanLetters )[i];
+      num -= valuesArr[i];
+    }
+  }
+  return romanNumber;
+};
