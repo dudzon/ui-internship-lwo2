df7decf31d565d6b8fc458c1eacb286d0ce7cbb6
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-05-07 14:35:01 +0200

    Task 89

---
+++ tasks/89_symmertic-difference/symmertic-difference.js
@@ -0,0 +1,25 @@
+/* eslint-disable max-len */
+const symDiff = (a, b) => {
+  const result = [];
+
+  a = [...new Set( a.sort() )];
+
+  b = [...new Set( b.sort() )];
+
+  a.map(( element ) => {
+    if ( b.indexOf( element ) === -1 ) {
+      result.push( element );
+    } else {
+      b.splice( b.indexOf(element), 1 );
+    }
+  });
+  return result.concat(b);
+};
+
+export const sym = (...args) => {
+  let symmetricDifference = symDiff( args[0], args[1] );
+  for ( let i = 2; i < args.length; i++ ) {
+    symmetricDifference = symDiff( symmetricDifference, args[i] );
+  }
+  return [...new Set( symmetricDifference.sort() )];
+};
