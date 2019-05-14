d42e011247816d6105522623215ab012cba227b8
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-05-14 13:55:07 +0200

    Task 77 - refactored

--- tasks/77_expressions-matter/expressions-matter.js
+++ tasks/77_expressions-matter/expressions-matter.js
@@ -3,15 +3,10 @@ export function expressionMatter( a, b, c ) {
   // eslint-disable-next-line prefer-rest-params
   const sortedArgs = Object.values(arguments).sort((a, b) => a-b );
   [a, b, c] = sortedArgs;
-
-  let result = [];
   const test1 = a + ( b * c );
   const test2 = a * b * c;
   const test3 = a + b * c;
   const test4 = ( a + b ) * c;
   const test5 = a + b + c;
-  result.push( test1, test2, test3, test4, test5);
-  return Math.max( ...result );
+  return Math.max(test1, test2, test3, test4, test5);
 }
-
-
