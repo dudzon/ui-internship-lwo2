5143103938f8d8fef16c8f16f1481fcefae14e3b
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-05-14 10:53:43 +0200

    Task 53 - refactored code

--- tasks/53_diff-array/diff-array.js
+++ tasks/53_diff-array/diff-array.js
@@ -1,23 +1,24 @@
+/* eslint-disable max-len */
 // It's not a smart solution :(
-// export const diffArray = ( arr1, arr2 ) => {
-//   let result = [];
+export const diffArray = ( arr1, arr2 ) => {
+  let result = [];
 
-//   arr1.filter(( item ) => {
-//     if (arr2.indexOf( item ) === -1) {
-//       result.push( item );
-//     }
-//   });
-//   arr2.filter(( item ) => {
-//     if (arr1.indexOf( item ) === -1) {
-//       result.push( item );
-//     }
-//   });
-//   return result;
-// };
+  arr1.forEach(( item ) => {
+    if (!arr2.includes( item )) {
+      result.push( item );
+    }
+  });
+  arr2.forEach(( item ) => {
+    if (!arr1.includes( item )) {
+      result.push( item );
+    }
+  });
+  return result;
+};
 
 // Solution 2 -  much better
 
-export const diffArray = ( arr1, arr2 ) => [...arr1, ...arr2]
-    // eslint-disable-next-line max-len
-    .filter(( elem, index, arr ) => arr.indexOf(elem) === arr.lastIndexOf(elem) );
+// export const diffArray = ( arr1, arr2 ) => [...arr1, ...arr2]
+//     // eslint-disable-next-line max-len
+//     .filter(( elem, index, arr ) => arr.indexOf(elem) === arr.lastIndexOf(elem) );
 
