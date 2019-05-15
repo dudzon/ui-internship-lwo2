373f260d441457a3859d380761afe87be5a44757
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-05-15 12:05:56 +0200

    Task 94 - refactored

--- tasks/94_calculate-orbital-period/calculate-orbital-period.js
+++ tasks/94_calculate-orbital-period/calculate-orbital-period.js
@@ -7,11 +7,9 @@ export const orbitalPeriod = ( satellites ) => {
     let totalRadius = earthRadius + satelliteAlt;
     let orbitalV = Math.sqrt( GM / totalRadius );
     let oPeriod = Math.round((( 2 * Math.PI ) * totalRadius ) / orbitalV );
-    data.velocity = orbitalV.toFixed(4);
-    data.orbitalPeriod = oPeriod;
     return {
       name: data.name,
-      orbitalPeriod: data.orbitalPeriod,
+      orbitalPeriod: oPeriod,
     };
   });
 };
