180651348a33fc41f25c0df13b308a7f2c4db1c1
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-05-15 09:32:07 +0200

    Task 71 -refactored

--- tasks/71_binary-encode/binary-encode.js
+++ tasks/71_binary-encode/binary-encode.js
@@ -1,11 +1,25 @@
 /* eslint-disable max-len */
+// export const binaryEncode = (str) => {
+//   if ( !str ) {
+//     return '';
+//   } else {
+//     return str.split('').map( (item) => item.charCodeAt(0).toString(2) )
+//         .map( (item) => item.length == 6 ? '00' + item.slice(0) : '0' + item.slice(0) )
+//         .join()
+//         .replace(/,/g, ' ');
+//   }
+// };
+
+//  Solution 2
 export const binaryEncode = (str) => {
   if ( !str ) {
     return '';
   } else {
-    return str.split('').map( (item) => item.charCodeAt(0).toString(2) )
-        .map( (item) => item.length == 6 ? '00' + item.slice(0) : '0' + item.slice(0) )
-        .join()
-        .replace(/,/g, ' ');
+    return str.split('')
+        .map( (item) => {
+          let binary = item.charCodeAt(0).toString(2);
+          return (binary.length == 6) ? binary.padStart(8, '00') : binary.padStart(8, '0');
+        })
+        .join(' ');
   }
 };
