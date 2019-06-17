68836c019fb24d1c13abf198508e2fc1e2dd9288
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-06-17 13:41:57 +0200

    Gulp setup, header finished

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const paths = {
+  styles: {
+    src: 'css_tasks/task9/scss/*.scss',
+    dest: 'css_tasks/task9/css/',
+  },
+  html: {
+    src: 'css_tasks/ task9/*.html',
+  },
+};
+function style() {
+  return (
+    gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream())
+  );
+}
+
+function watch() {
+  browserSync.init({
+    watch: true,
+    server: {
+      baseDir: './css_tasks/task9',
+    },
+  });
+  gulp.watch(paths.styles.src, style);
+}
+
+exports.style = style;
+exports.watch = watch;
+
