4aa0f8e77829f8dd80becbbd4580cae121903b3f
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-07-11 10:26:55 +0200

    Changed name for the jumbotron form

--- css_tasks/task9/js/app.js
+++ css_tasks/task9/js/app.js
@@ -1,1 +1,9 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+const jumbotronForm = document.forms.jumbotronForm;
 
+jumbotronForm.addEventListener('submit', (e) => {
+  const jumbotronName = jumbotronForm.elements.name.value;
+  e.preventDefault();
+  console.log(jumbotronName);
+});
