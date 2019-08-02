ea5f2690fb86aaa0f73cf428f8cbf399d51efb99
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-08-02 10:17:33 +0200

    Refactored events

--- css_tasks/task9/js/forms.js
+++ css_tasks/task9/js/forms.js
@@ -16,48 +16,57 @@ export function form() {
     } = constants;
     const {loginForm, contactForm} = data;
     const submitLoginForm = (e) => {
-      validate(e.target, e, 'name', namePattern, nameText);
-      validate(e.target, e, 'email', emailPattern, emailText);
-      validate(e.target, e, 'password', passwordPattern, passwordText);
+      e.preventDefault();
+      validate(e.target, 'name', namePattern, nameText);
+      validate(e.target, 'email', emailPattern, emailText);
+      validate(e.target, 'password', passwordPattern, passwordText);
+      checkErrors(e.target);
     };
     const submitContactForm = (e) => {
-      validate(e.target, e, 'name', namePattern, nameText);
-      validate(e.target, e, 'email', emailPattern, emailText);
-      validate(e.target, e, 'message', null, messageText);
+      e.preventDefault();
+      validate(e.target, 'name', namePattern, nameText);
+      validate(e.target, 'email', emailPattern, emailText);
+      validate(e.target, 'message', null, messageText);
+      checkErrors(e.target);
     };
 
     // Validate input
 
-    const validate = (element, e, string, pattern, inputText) => {
+    const validate = (element, string, pattern, inputText) => {
       const input = element.elements[string];
       const inputValue = input.value;
-      hideWarning(input);
+      hideWarning(input, element);
       if (
         inputValue.length < 1 ||
         inputValue.length > 250 ||
         (pattern && !pattern.test(inputValue))
       ) {
-        showWarning(input, inputText);
-        e.preventDefault();
+        showWarning(input, inputText, element);
       }
     };
     // Show warning message when form fails validation
 
-    const showWarning = (formName, text) => {
+    const showWarning = (formName, text, form) => {
       const warningMessage = document
           .createRange()
           .createContextualFragment(`<p class="form-error">${text}</p>`);
+      form.setAttribute('data-error', 'error');
       formName.after(warningMessage);
     };
 
     // Hide warning message if the input passes validation
 
-    const hideWarning = (elem) => {
+    const hideWarning = (elem, form) => {
       const warning = elem.nextElementSibling;
       if (warning && warning.classList.contains('form-error')) {
         warning.remove();
+        form.removeAttribute('data-error');
       }
     };
+    const checkErrors = (element) => {
+      if (!element.hasAttribute('data-error')) window.location.reload();
+    };
+
     loginForm.addEventListener('submit', submitLoginForm);
     contactForm.addEventListener('submit', submitContactForm);
   });
