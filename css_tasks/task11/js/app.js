5ab24f67622b17fdbb97356bc351db24611cc292
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-08-11 11:08:27 +0200

    changef gulpfile.js and app.js

--- css_tasks/task11/js/app.js
+++ css_tasks/task11/js/app.js
@@ -1,11 +1,10 @@
 import {accordion} from './accordion.js';
-accordion();
 import {popup} from './popup.js';
-
-popup();
 import {scroll} from './scroll.js';
-scroll();
 import {slider} from './slider.js';
-slider();
 import {fetchData} from './fetch.js';
+accordion();
+popup();
+scroll();
+slider();
 fetchData();
--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 const paths = {
   styles: {
-    src: 'css_tasks/task10/scss/*.scss',
-    dest: 'css_tasks/task10/css/',
+    src: 'css_tasks/task11/scss/*.scss',
+    dest: 'css_tasks/task11/css/',
   },
   html: {
-    src: 'css_tasks/task10/*.html',
+    src: 'css_tasks/task11/*.html',
   },
   js: {
-    src: 'css_tasks/task10/*.html',
+    src: 'css_tasks/task11/*.html',
   },
 };
 function style() {
@@ -33,7 +33,7 @@ function watch() {
   browserSync.init({
     watch: true,
     server: {
-      baseDir: './css_tasks/task10',
+      baseDir: './css_tasks/task11',
     },
   });
   gulp.watch(paths.styles.src, style);
